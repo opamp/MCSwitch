7137cff989d603bd037e4be5d9a81a1110c30770
opamp sando <opampg@gmail.com>
2012-08-22 18:37:03 +0900

    version.hppにLicense情報追加

--- src/version.hpp
+++ src/version.hpp
@@ -3,5 +3,5 @@
 
 const QString app_name = "MCSwitch" // Application Full Name.
 const QString app_ver = "0.0.1-dev" // Application Version.
-
+const QString app_license = "MIT" // Application License.
 
