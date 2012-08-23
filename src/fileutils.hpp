a3ddeff4fa8e100f79fd30cf9cc66a2adc3a0e1d
opamp sando <opampg@gmail.com>
2012-08-23 21:53:03 +0900

    デバッグ用のcout等を削除。今日の作業はここまでかな...

--- src/fileutils.hpp
+++ src/fileutils.hpp
@@ -6,8 +6,6 @@
 #include<QFileInfo>
 #include<QDir>
 
-#include<iostream>
-
 namespace fileutils{
 	/*Directoryのコピーを行う*/
     bool cp_R(const QString from,const QString to){
@@ -22,16 +20,13 @@ namespace fileutils{
         while(i.hasNext()){
             b = i.next();
             bf = from + "/" + b;
-            std::cout<<"=="<<bf.toStdString()<<std::endl;
+            //std::cout<<"=="<<bf.toStdString()<<std::endl;
             if(bf == from + "/" + QString(".") or bf == from + "/" + QString("..")){
-                std::cout<<"==SKIP"<<std::endl;
                 continue;
             }
             if(QFileInfo(bf).isDir()){
-                std::cout<<"qfileinfo id dir\n";
                 cp_R(bf,to + "/" + b);
             }else{
-                std::cout<<"qfileinfo id is not dir."<<std::endl;
                 if(!QFile::copy(bf,to + "/" + b)) return false;
 			}
 		}
