e33b85eceda777b45ff4aed2bf1d8f1f3d49392d
opamp sando <opampg@gmail.com>
2012-08-31 19:44:54 +0900

    xmlパースのopen部分を書いた

--- src/xml.cpp
+++ src/xml.cpp
@@ -7,6 +7,43 @@ Xml::Xml(){
 bool Xml::open(const QString filename){
     if(!QFile::exists(filename)) return false;
     this->filename = filename;
+    QDomDocument doc;
+    QString error;int errorline;int errorColmun;
+    QString rootTagName = "MCSwitch_Env";
+    QFile f(this->filename);
+
+    if(f.open(QIODevice::ReadOnly)){
+        //fail to open file.
+        return false;
+    }
+
+    if(!doc.setContent(&f,true,&error,&errorline,&errorColmun)){
+        //fail to set content.
+        return false;
+    }
+    QDomElement root = doc.documentElement();
+    if(root.tagName().toStdString() != rootTagName){
+        return false;
+    }
+
+    QDomNode node = root.firstChild();
+    while(!node.isNull()){
+        if(node.toElement().tagName().toStdString() == "name"){
+
+        }
+        else if(node.toElement().tagName().toStdString() == "version"){
+
+        }
+        else if(node.toElement().tagName().toStdString() == "mods"){
+
+        }
+        else if(node.toElement().tagName().toStdString() == "comment"){
+
+        }
+        else{
+
+        }
+    }
 
     return true;
 }
