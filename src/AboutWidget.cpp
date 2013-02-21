4ebb13cf16e33692e21f7fb47614681983574cdc
opamp sando <opampg@gmail.com>
2013-02-22 00:20:35 +0900

    新しいクラスを追加

---
+++ src/AboutWidget.cpp
@@ -0,0 +1,6 @@
+#include"AboutWidget.hpp"
+
+AboutWidget::AboutWidget(QWidget* parent):
+    QWidget(parent){
+
+}
---
+++ src/AboutWidget.hpp
@@ -0,0 +1,21 @@
+#ifndef ABOUT_WIDGET_HPP
+#define ABOUT_WIDGET_HPP
+#include<QWidget>
+#include<QTextEdit>
+#include<QPushButton>
+#include<QLabel>
+
+class AboutWidget : public QWidget{
+    Q_OBJECT
+public:
+    AboutWidget(QWidget* parent = 0);
+
+private:
+    QLabel* appNameLabel;
+    QLabel* versionLabel;
+    QLabel* urlLabel;
+    QTextEdit* licenseViewer;
+    QPushButton* OKButton;
+};
+
+#endif
