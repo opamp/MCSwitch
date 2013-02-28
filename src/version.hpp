31beff86cf4246a1860031a11c43fb607fbcc208
opamp <opampg@gmail.com>
2013-02-28 18:47:46 +0900

    表示を0.0.4-devにした。まあ、まだreleaseじゃないけど

--- src/version.hpp
+++ src/version.hpp
@@ -8,7 +8,7 @@
 const QString fsp = QDir::separator();
 
 const QString app_name = "MCSwitch"; // Application Full Name.
-const QString app_ver = "0.0.3-dev"; // Application Version.
+const QString app_ver = "0.0.4-dev"; // Application Version.
 const QString app_license = "MIT"; // Application License.
 
 const QString mcswitch_dir = QDir::homePath() + fsp + ".MCSwitch"; //Application data dir.
