8dfe507c36dbf4dd533c9efb0927901a94f98892
opamp <opampg@gmail.com>
2013-07-28 21:49:29 +0900

    uninstallシェルスクリプトを追加

---
+++ uninstall.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+if [ -L $HOME/Library/Application\ Support/minecraft ];then
+	rm -fr $HOME/Library/Application\ Support/minecraft
+	mv $HOME/.MCSwitch/loadingenv $HOME/Library/Application\ Support/minecraft
+	mv $HOME/.MCSwitch/env $HOME/Desktop/MCSwitchEnvs
+	rm -f $HOME/Library/Application\ Support/minecraft/.mcswitch.xml
+	rm -fr $HOME/.MCSwitch
+	echo "FINISHED"
+else
+	echo "ERROR"
+fi
+
