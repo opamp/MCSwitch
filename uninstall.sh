9d17780edbf46969380f438ffd5cca2d9748f91c
opamp <opampg@gmail.com>
2014-06-16 23:53:45 +0900

    uninstall.shをlinuxに対応させてみた.動作テストしてない

--- uninstall.sh
+++ uninstall.sh
@@ -1,14 +1,30 @@
 #!/bin/zsh
 
-if [ -L $HOME/Library/Application\ Support/minecraft ];then
-	rm -fr $HOME/Library/Application\ Support/minecraft
-	mv $HOME/.MCSwitch/loadingenv $HOME/Library/Application\ Support/minecraft
-	mv $HOME/.MCSwitch/env $HOME/Desktop/MCSwitchEnvs
-	rm -f $HOME/Library/Application\ Support/minecraft/.mcswitch.xml
-	rm -fr $HOME/.MCSwitch
-	rm -f $HOME/MCSwitchEnvironments
-	echo "FINISHED"
+UNAME=`uname`
+if test ${UNAME} = "Darwin";then #DARWIN
+    if [ -L $HOME/Library/Application\ Support/minecraft ];then
+        rm -fr $HOME/Library/Application\ Support/minecraft
+        mv $HOME/.MCSwitch/loadingenv $HOME/Library/Application\ Support/minecraft
+        mv $HOME/.MCSwitch/env $HOME/Desktop/MCSwitchEnvs
+        rm -f $HOME/Library/Application\ Support/minecraft/.mcswitch.xml
+        rm -fr $HOME/.MCSwitch
+        rm -f $HOME/MCSwitchEnvironments
+        echo "FINISHED"
+    else
+        echo "ERROR"
+    fi
+elif test ${UNAME} = "Linux";then #LINUX
+    if [ -L $HOME/.minecraft ];then
+        rm -fr $HOME/.minecraft
+        mv $HOME/.MCSwitch/loadingenv $HOME/.minecraft
+        mv $HOME/.MCSwitch/env $HOME/MCSwitchEnvsBackup
+        rm -f $HOME/.minecraft/.mcswitch.xml
+        rm -fr $HOME/.MCSwitch
+        rm -f $HOME/MCSwitchEnvironments
+        echo "FINISHED"
+    else
+        echo "ERROR"
+    fi
 else
-	echo "ERROR"
+    echo "not supported"
 fi
-
